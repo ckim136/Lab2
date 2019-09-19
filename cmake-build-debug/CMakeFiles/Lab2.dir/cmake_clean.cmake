3c67cab767f515b6d5e4df937ad3f26591aae011
Chaeeun Kim <kimemily1227@gmail.com>
2019-09-18 21:00:19 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Lab2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab2.dir/main.cpp.o"
+  "Lab2.pdb"
+  "Lab2.exe"
+  "libLab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
